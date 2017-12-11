ec6ef27b98bb2e711d1f381be3ebbf8ffea36e34
Luke Meszsar <lume0018@colorado.edu>
2017-12-10 20:30:40 -0700

    Final code

--- HW4/neighborhood.py
+++ HW4/neighborhood.py
@@ -9,15 +9,19 @@ class Neighborhood:
         self.neighborhood = [0 for x in range(60)]
 
     def generate_neighborhood(self):
+        #create a random configuration of a neighborhood
         neighbor_indices = [x for x in range(60)]
         random.shuffle(neighbor_indices)
         for i in range(60):
             current_house = neighbor_indices[i]
             if i <= 5:
+                #create 5 empty houses
                 self.neighborhood[current_house] = 0
             elif i > 5 and i <= 32:
+                #create 27 blue houses
                 self.neighborhood[current_house] = 1
             else:
+                #create 27 red houses
                 self.neighborhood[current_house] = 2
     def is_dissatisfied(self,index):
         current_house_type = self.neighborhood[index]
@@ -30,13 +34,16 @@ class Neighborhood:
             neighbor_types.append(self.neighborhood[(index+offset)%60])
             #mod 60 allows for wrapping around the ends of the array
         if neighbor_types.count(current_house_type) >= 2:
+            #want at least two neighbors of the same type
             return False
         else:
             return True
     def find_dissatisfied(self):
+        #get list of all dissatisfied families
         dissatisfied_families = []
         for i in range(60):
             if self.is_dissatisfied(i):
+                #appends "address"
                 dissatisfied_families.append(i)
         return dissatisfied_families
 
@@ -47,14 +54,17 @@ class Neighborhood:
             return True
         #get "address" of dissatisfied house in neighborhood
         random_dissatisfied_index = dissatisfied_families[random.randrange(0,len(dissatisfied_families))]
+        #find all empty houses
         indices_of_empty_houses = indices = [i for i, x in enumerate(self.neighborhood) if x == 0]
         #get "address" of empty house in neighborhood
         random_empty_house_index = indices_of_empty_houses[random.randrange(0,len(indices_of_empty_houses))]
+        #move family into empty house
         self.neighborhood[random_empty_house_index] = self.neighborhood[random_dissatisfied_index]
         self.neighborhood[random_dissatisfied_index] = 0
         return False
 
     def create_image(self):
+        #code that generates images
         nrows, ncols = 1,60
         image = np.zeros(nrows*ncols)
         for i in range(len(self.neighborhood)):
@@ -80,6 +90,7 @@ class Neighborhood:
             if is_done:
                 break
             if i % 5 == 0:
+                #get image every 5 time steps
                 self.create_image()
 
 
--- HW4/nn.py
+++ HW4/nn.py
@@ -3,7 +3,8 @@ import random as rnd
 import pickle
 
 class Network:
-    def __init__(self, training_prop=0.8,alpha=0.1):
+    def __init__(self, training_prop=0.8,alpha=0.5):
+        #randomize weights
         self.weights = [rnd.uniform(-1,1) for n in range(3)]
         self.bias = - 1
         self.training_prop = training_prop
@@ -15,61 +16,88 @@ class Network:
         with open ('y_data', 'rb') as fp:
             y_data = pickle.load(fp)
         c = list(zip(x_data, y_data))
+        #shuffle x and y data together
         rnd.shuffle(c)
         x_data, y_data = zip(*c)
         split_i = int(len(x_data)*training_prop)
+        #create training and testing data
         self.x_data_train = x_data[0:split_i]
         self.x_data_test = x_data[split_i:]
         self.y_data_train = y_data[0:split_i]
         self.y_data_test = y_data[split_i:]
     def forward_prop(self, a):
         #used for testing
-        #print("weight",self.weights)
-        #print("a",a)
-        a = self.g(np.dot(self.weights,a)+self.bias)
-        #print("new a",a)
-        return a
+        return self.g(np.dot(self.weights,a)+self.bias)
 
     def back_prop(self,x,y):
         dW_list = np.zeros(3)
         a = x
+        #first forward prop
         a = self.g(np.dot(self.weights,a)+self.bias)
         for i in range(3):
-            dW_list[i] = dW_list[i] + (self.alpha*(y-a)*self.g_prime(a)*x[i])
+            #update each weight via formula from slides
+            dW_list[i] = self.weights[i] + (self.alpha*(y-(np.round(a)))*self.g_prime(a)*x[i])
         return dW_list
 
-    def SGD(self):
-        counter = 0
-        for (x,y) in zip(self.x_data_train, self.y_data_train):
-            self.weights = self.back_prop(x,y)
-            if counter % 250 == 0:
-                print(self.weights)
-            counter += 1
+    def SGD(self,epochs=1):
+        #function performs stochastic gradient desccent to update weights
+        n_train = len(self.x_data_train)
+        #implemented epoch based training
+        for epoch in range(epochs):
+            print(epoch)
+            perm = np.random.permutation(n_train)
+            counter = 0
+            for kk in range(n_train):
+                #get x and y instance
+                xk = self.x_data_train[perm[kk]]
+                yk = self.y_data_train[perm[kk]]
+                if counter % 250 == 0 or counter <= 100:
+                    print(counter, self.weights)
+                counter += 1
+                #update weights
+                self.weights = self.back_prop(xk,yk)
     def evaluate(self):
-        for (x,y) in zip(self.x_data_train,self.y_data_train):
-            print("a", self.forward_prop(x), "y", y)
-
+        ctr = 0
+        for (x,y) in zip(self.x_data_test,self.y_data_test):
+            ctr += np.round(self.forward_prop(x)) == y
+            #+1 if correctly evaluated
+        #return testing accuracy
+        return float(ctr) / float(len(self.y_data_test))
     def g(self, z):
-        """
-        activation function
-        """
+        #activation function
         return self.sigmoid(z)
 
     def g_prime(self, z):
-        """
-        derivative of activation function
-        """
+        #derivative of activation function
         return self.sigmoid_prime(z)
 
     def sigmoid(self,z, threshold=20):
-        z = np.clip(z, -threshold, threshold)
         return 1.0/(1.0+np.exp(-z))
 
     def sigmoid_prime(self, z):
         return self.sigmoid(z) * (1.0 - self.sigmoid(z))
 
 
+def best_alpha():
+    #function sweeps through a list of alphas and finds best one
+    possible_alphas = np.linspace(0.01,5,100)
+    best_alpha_val = 0
+    best_test_score = 0
+    for pos_alp in possible_alphas:
+        print("current alpha", pos_alp)
+        nn = Network(training_prop=0.8,alpha=pos_alp)
+        nn.SGD(epochs=1)
+        score = nn.evaluate()
+        if score == 1.0:
+        if score > best_test_score:
+            best_test_score = score
+            best_alpha_val = pos_alp
+
+    print("best_alpha", best_alpha_val, "best_test_score", best_test_score)
+
+
 if __name__ == '__main__':
-    nn = Network(training_prop=0.8)
-    nn.SGD()
-    nn.evaluate()
+    nn = Network(training_prop=0.8,alpha=0.01)
+    nn.SGD(epochs=1)
+    print(nn.evaluate())
+    #best_alpha()
